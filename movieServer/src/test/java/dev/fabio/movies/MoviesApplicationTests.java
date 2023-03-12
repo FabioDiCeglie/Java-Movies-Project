a8b0f00d69fd69fa7798a5057545d46470f03924
FabioDiCeglie <dcfabio96@gmail.com>
2023-03-12 10:18:59 +0100

    chore: modify tree

---
+++ movieServer/src/test/java/dev/fabio/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fabio.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

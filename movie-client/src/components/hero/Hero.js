119691a28f96779de9914ccbcb8959153373a2d4
FabioDiCeglie <dcfabio96@gmail.com>
2023-03-15 17:03:06 +0100

    feat: implement button trailer in the app

--- movie-client/src/components/hero/Hero.js
+++ movie-client/src/components/hero/Hero.js
@@ -3,6 +3,8 @@ import Carousel from "react-material-ui-carousel";
 import { Paper } from "@mui/material";
 import { useGetMovies } from "../../api/hooks";
 import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 
 function Hero() {
   const [movies, setMovies] = useState([]);
@@ -31,6 +33,14 @@ function Hero() {
                     </div>
                     <div className="movie-title">
                       <h4>{movie.title}</h4>
+                    </div>
+                    <div className="movie-buttons-container">
+                      <div className="play-button-icon-container">
+                        <FontAwesomeIcon
+                          className="play-button-icon"
+                          icon={faCirclePlay}
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>

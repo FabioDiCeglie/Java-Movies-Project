464f17d9d528736e56d8842c7bcfd843ac393813
FabioDiCeglie <dcfabio96@gmail.com>
2023-03-15 09:52:06 +0100

    feat: start working on App.js

--- movie-client/src/App.js
+++ movie-client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useGetMovies } from "./api/hooks";
+import { useState, useEffect } from "react";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () =>
+    await useGetMovies().then((data) => setMovies(data));
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  console.log("MOVIES", movies);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={Layout}></Route>
+      </Routes>
     </div>
   );
 }
